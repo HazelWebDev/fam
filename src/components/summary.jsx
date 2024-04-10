9010d855fc547578a08d30947c03fc2d9d007d3c
Kristen Roggero <kristenroggero@Kristens-Laptop.local>
2024-04-10 10:18:31 -0500

    Adjusted apostophes

--- src/components/summary.jsx
+++ src/components/summary.jsx
@@ -21,7 +21,7 @@ export default function Page() {
             Long-term thinking
           </h2>
           <p className="mt-3 text-xl text-white">
-            We're committed to responsible, sustainable, and ethical
+            We&rsquo;re committed to responsible, sustainable, and ethical
             manufacturing. Our small-scale approach allows us to focus on
             quality and reduce our impact. We're doing our best to delay the
             inevitable heat-death of the universe.
